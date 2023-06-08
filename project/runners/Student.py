69b7cbd1e74b0f3309567f16582e0e9e555384ca
Tonio_Dev <antoine.deshayes@telecom-sudparis.eu>
2023-06-08 23:34:30 +0200

    groupement des fichiers

---
+++ project/runners/Student.py
@@ -0,0 +1,400 @@
+import asyncio
+import base64
+import binascii
+import json
+import logging
+import os
+import sys
+import time
+import datetime
+from urllib.parse import urlparse
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from runners.agent_container_v2 import (  # noqa:E402
+    arg_parser,
+    create_agent_with_args,
+    AriesAgent,
+)
+
+from runners.support.agent import (  # noqa:E402
+    CRED_FORMAT_INDY,
+    CRED_FORMAT_JSON_LD,
+    SIG_TYPE_BLS,
+)
+
+from runners.support.utils import (  # noqa:E402
+    check_requires,
+    log_msg,
+    log_status,
+    log_timer,
+    log_json,
+    prompt,
+    prompt_loop,
+)
+
+logging.basicConfig(level=logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+CRED_PREVIEW_TYPE = "https://didcomm.org/issue-credential/2.0/credential-preview"
+
+
+class StudentAgent(AriesAgent):
+    def __init__(
+        self,
+        ident: str,
+        http_port: int,
+        admin_port: int,
+        no_auto: bool = False,
+        aip: int = 20,
+        endorser_role: str = None,
+        **kwargs,
+    ):
+        super().__init__(
+            ident,
+            http_port,
+            admin_port,
+            prefix="Student",
+            no_auto=no_auto,
+            seed=None,
+            aip=aip,
+            endorser_role=endorser_role,
+            **kwargs,
+        )
+        self.connection_id = None
+        self._connection_ready = None
+        self.cred_state = {}
+        self.cred_attrs = {}
+
+    async def detect_connection(self):
+        await self._connection_ready
+        self._connection_ready = None
+
+    @property
+    def connection_ready(self):
+        return self._connection_ready.done() and self._connection_ready.result()
+
+    def generate_credential_proposal(self, aip, cred_type, cred_def_id, exchange_tracing, name, age):
+        d = datetime.date.today()
+        birth_date = datetime.date(d.year - int(age), d.month, d.day)
+        date_format = "%Y%m%d"
+        if aip == 10:
+            # define attributes to send for credential
+            self.cred_attrs[cred_def_id] = {
+                "name": name,
+                "date": d.strftime(date_format),
+                "degree": "Ingénieur généraliste du numérique",
+                "birthdate_dateint": birth_date.strftime(date_format),
+                "timestamp": str(int(time.time())),
+            }
+
+            cred_preview = {
+                "@type": CRED_PREVIEW_TYPE,
+                "attributes": [
+                    {"name": n, "value": v}
+                    for (n, v) in self.cred_attrs[cred_def_id].items()
+                ],
+            }
+            proposal_request = {
+                "connection_id": self.connection_id,
+                "cred_def_id": cred_def_id,
+                "comment": f"Offer on cred def id {cred_def_id}",
+                "auto_remove": False,
+                "credential_preview": cred_preview,
+                "trace": exchange_tracing,
+            }
+            return proposal_request
+
+        elif aip == 20:
+            if cred_type == CRED_FORMAT_INDY:
+                self.cred_attrs[cred_def_id] = {
+                    "name": name,
+                    "date": d.strftime(date_format),
+                    "degree": "Ingenieur generaliste du numerique",
+                    "birthdate_dateint": birth_date.strftime(date_format),
+                    "timestamp": str(int(time.time())),
+                }
+
+                cred_preview = {
+                    "@type": CRED_PREVIEW_TYPE,
+                    "attributes": [
+                        {"name": n, "value": v}
+                        for (n, v) in self.cred_attrs[cred_def_id].items()
+                    ],
+                }
+                offer_request = {
+                    "connection_id": self.connection_id,
+                    "comment": f"Offer on cred def id {cred_def_id}",
+                    "auto_remove": False,
+                    "credential_preview": cred_preview,
+                    "filter": {"indy": {"cred_def_id": cred_def_id}},
+                    "trace": exchange_tracing,
+                }
+                return offer_request
+
+            else:
+                raise Exception(f"Error invalid credential type: {self.cred_type}")
+
+        else:
+            raise Exception(f"Error invalid AIP level: {self.aip}")
+
+
+async def input_invitation(agent_container):
+    agent_container.agent._connection_ready = asyncio.Future()
+    async for details in prompt_loop("Invite details: "):
+        b64_invite = None
+        try:
+            url = urlparse(details)
+            query = url.query
+            if query and "c_i=" in query:
+                pos = query.index("c_i=") + 4
+                b64_invite = query[pos:]
+            elif query and "oob=" in query:
+                pos = query.index("oob=") + 4
+                b64_invite = query[pos:]
+            else:
+                b64_invite = details
+        except ValueError:
+            b64_invite = details
+
+        if b64_invite:
+            try:
+                padlen = 4 - len(b64_invite) % 4
+                if padlen <= 2:
+                    b64_invite += "=" * padlen
+                invite_json = base64.urlsafe_b64decode(b64_invite)
+                details = invite_json.decode("utf-8")
+            except binascii.Error:
+                pass
+            except UnicodeDecodeError:
+                pass
+
+        if details:
+            try:
+                details = json.loads(details)
+                break
+            except json.JSONDecodeError as e:
+                log_msg("Invalid invitation:", str(e))
+
+    with log_timer("Connect duration:"):
+        connection = await agent_container.input_invitation(details, wait=True)
+
+
+async def main(args):
+    student_agent = await create_agent_with_args(args, ident="student")
+
+    try:
+        log_status(
+            "#7 Provision an agent and wallet, get back configuration details"
+            + (
+                f" (Wallet type: {student_agent.wallet_type})"
+                if student_agent.wallet_type
+                else ""
+            )
+        )
+        agent = StudentAgent(
+            "student.agent",
+            student_agent.start_port,
+            student_agent.start_port + 1,
+            genesis_data=student_agent.genesis_txns,
+            genesis_txn_list=student_agent.genesis_txn_list,
+            no_auto=student_agent.no_auto,
+            tails_server_base_url=student_agent.tails_server_base_url,
+            revocation=student_agent.revocation,
+            timing=student_agent.show_timing,
+            multitenant=student_agent.multitenant,
+            mediation=student_agent.mediation,
+            wallet_type=student_agent.wallet_type,
+            aip=student_agent.aip,
+            endorser_role=student_agent.endorser_role,
+        )
+
+        await student_agent.initialize(the_agent=agent)
+
+        log_status("#9 Input Issuer invitation details")
+        await input_invitation(student_agent)
+
+        options = "    (1) Send Message\n" "    (2) Input New Invitation\n" "    (3) See the received credentials\n" "    (4) Credential request\n" "    (5) DIDs list\n"
+        if student_agent.endorser_role and student_agent.endorser_role == "author":
+            options += "    (D) Set Endorser's DID\n"
+        if student_agent.multitenant:
+            options += "    (W) Create and/or Enable Wallet\n"
+        options += "    (X) Exit?\n[1/2/3/4/5/{}X] ".format(
+            "W/" if student_agent.multitenant else "",
+        )
+        async for option in prompt_loop(options):
+            if option is not None:
+                option = option.strip()
+
+            if option is None or option in "xX":
+                break
+
+            elif option in "dD" and student_agent.endorser_role:
+                endorser_did = await prompt("Enter Endorser's DID: ")
+                await student_agent.agent.admin_POST(
+                    f"/transactions/{student_agent.agent.connection_id}/set-endorser-info",
+                    params={"endorser_did": endorser_did, "endorser_name": "endorser"},
+                )
+
+            elif option in "wW" and student_agent.multitenant:
+                target_wallet_name = await prompt("Enter wallet name: ")
+                include_subwallet_webhook = await prompt(
+                    "(Y/N) Create sub-wallet webhook target: "
+                )
+                if include_subwallet_webhook.lower() == "y":
+                    await student_agent.agent.register_or_switch_wallet(
+                        target_wallet_name,
+                        webhook_port=student_agent.agent.get_new_webhook_port(),
+                        mediator_agent=student_agent.mediator_agent,
+                        taa_accept=student_agent.taa_accept,
+                    )
+                else:
+                    await student_agent.agent.register_or_switch_wallet(
+                        target_wallet_name,
+                        mediator_agent=student_agent.mediator_agent,
+                        taa_accept=student_agent.taa_accept,
+                    )
+
+            elif option == "1":
+                msg = await prompt("Enter message: ")
+                if msg:
+                    await student_agent.agent.admin_POST(
+                        f"/connections/{student_agent.agent.connection_id}/send-message",
+                        {"content": msg},
+                    )
+
+            elif option == "2":
+                # handle new invitation
+                log_status("Input new invitation details")
+                await input_invitation(student_agent)
+
+            elif option == "3":
+                credentials = await student_agent.agent.admin_GET(
+                    "/credentials"
+                )
+                log_json(credentials, label="Credential List")
+
+            elif option == "4":
+                cred_def_id = await prompt("Give credential definition: ")
+                exchange_tracing = False
+                name = await prompt("Fullname: ")
+                age = await prompt("Age: ")
+
+                if student_agent.aip == 10:
+
+                    proposal_request = student_agent.agent.generate_credential_proposal(
+                        student_agent.aip, None, cred_def_id, exchange_tracing, name, age
+                    )
+                    await student_agent.agent.admin_POST(
+                        "/issue-credential/send-proposal", proposal_request
+                    )
+
+                elif student_agent.aip == 20:
+                    proposal_request = student_agent.agent.generate_credential_proposal(
+                        student_agent.aip,
+                        CRED_FORMAT_INDY,
+                        cred_def_id,
+                        exchange_tracing,
+                        name,
+                        age,
+                    )
+
+                    log_status("Envoi de la requête")
+
+                    await student_agent.agent.admin_POST(
+                        "/issue-credential-2.0/send-proposal",
+                        proposal_request,
+                    )
+
+                else:
+                    raise Exception(
+                        "Error invalid credential type"
+                    )
+
+            elif option == "5":
+                list_did = await student_agent.agent.admin_GET(
+                    "/wallet/did"
+                )
+                log_status("Wallet registered DIDs")
+                log_json(list_did)
+                options_2 = (
+                    "    (N) Create a new DID\n"
+                    "    (X) Exit?\n"
+                )
+
+                async for option in prompt_loop(options_2):
+                    if option is not None:
+                        option = option.strip()
+
+                    if option in "nN":
+                        schema_did =    {
+                                          "method": "sov",
+                                          "options": {
+                                            "key_type": "ed25519"
+                                          },
+                                        }
+                        await student_agent.agent.admin_POST(
+                            "/wallet/did/create",
+                            schema_did
+                        )
+                        log_msg("New DID created!")
+                        break
+
+                    elif option is None or option in "xX":
+                        break
+
+                    else:
+                        raise Exception(f"Invalid option: {option}")
+
+        if student_agent.show_timing:
+            timing = await student_agent.agent.fetch_timing()
+            if timing:
+                for line in student_agent.agent.format_timing(timing):
+                    log_msg(line)
+
+    finally:
+        terminated = await student_agent.terminate()
+
+    await asyncio.sleep(0.1)
+
+    if not terminated:
+        os._exit(1)
+
+
+if __name__ == "__main__":
+    parser = arg_parser(ident="student", port=8030)
+    args = parser.parse_args()
+
+    ENABLE_PYDEVD_PYCHARM = os.getenv("ENABLE_PYDEVD_PYCHARM", "").lower()
+    ENABLE_PYDEVD_PYCHARM = ENABLE_PYDEVD_PYCHARM and ENABLE_PYDEVD_PYCHARM not in (
+        "false",
+        "0",
+    )
+    PYDEVD_PYCHARM_HOST = os.getenv("PYDEVD_PYCHARM_HOST", "localhost")
+    PYDEVD_PYCHARM_CONTROLLER_PORT = int(
+        os.getenv("PYDEVD_PYCHARM_CONTROLLER_PORT", 5001)
+    )
+
+    if ENABLE_PYDEVD_PYCHARM:
+        try:
+            import pydevd_pycharm
+
+            print(
+                "Student remote debugging to "
+                f"{PYDEVD_PYCHARM_HOST}:{PYDEVD_PYCHARM_CONTROLLER_PORT}"
+            )
+            pydevd_pycharm.settrace(
+                host=PYDEVD_PYCHARM_HOST,
+                port=PYDEVD_PYCHARM_CONTROLLER_PORT,
+                stdoutToServer=True,
+                stderrToServer=True,
+                suspend=False,
+            )
+        except ImportError:
+            print("pydevd_pycharm library was not found")
+
+    check_requires(args)
+
+    try:
+        asyncio.get_event_loop().run_until_complete(main(args))
+    except KeyboardInterrupt:
+        os._exit(1)
